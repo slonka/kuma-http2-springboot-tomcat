7f827db14cd3205770569abdbb3bad4e5e2d0f2b
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-04-26 15:56:47 +0800

    Add a delay before respond
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
 
 @SpringBootApplication
 @RestController
@@ -11,8 +12,22 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
+
+    private final Random random = new Random();
+
     @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+      int minSleepMillis = 1000; // 1 second
+      int maxSleepMillis = 5000; // 5 seconds
+
+      int sleepMillis = random.nextInt(maxSleepMillis - minSleepMillis + 1) + minSleepMillis;
+      try {
+        Thread.sleep(sleepMillis);
+      }
+      catch (InterruptedException e) {
+          return "Sleep was interrupted";
+      }
+
+      return String.format("4.10 Hello %s!", name);
     }
 }
